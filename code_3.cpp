1fd1773cb803b1de1e5e9e69f42f3d29fcd38143
bharath-99p <113783967+bharath-99p@users.noreply.github.com>
2024-03-09 16:46:32 +0530

    code_3.cpp

--- code_3.cpp
+++ code_3.cpp
@@ -1,234 +1,157 @@
-#include <NewPing.h>
-#include <Servo.h>
-#include <AFMotor.h>
+#define enA 10//Enable1 L298 Pin enA 
+#define in1 9 //Motor1  L298 Pin in1 
+#define in2 8 //Motor1  L298 Pin in1 
+#define in3 7 //Motor2  L298 Pin in1 
+#define in4 6 //Motor2  L298 Pin in1 
+#define enB 5 //Enable2 L298 Pin enB 
+#define L_S A0 //ir sensor Left
+#define R_S A1 //ir sensor Right
+#define echo A2    //Echo pin
+#define trigger A3 //Trigger pin
+#define servo A5
+#define rxPin  2
+#define txPin  3
+#include <SoftwareSerial.h>
+// Set up a new SoftwareSerial object
+SoftwareSerial mySerial (rxPin, txPin);
 
-//hc-sr04 sensor
-#define TRIGGER_PIN A2
-#define ECHO_PIN A3
-#define max_distance 50
-
-//ir sensor
-#define irLeft A0
-#define irRight A1
-
-//motor
-#define MAX_SPEED 200
-#define MAX_SPEED_OFFSET 20
-
-Servo servo;
-String plant_id;
-
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, max_distance);
-
-AF_DCMotor motor1(1, MOTOR12_1KHZ);
-AF_DCMotor motor2(2, MOTOR12_1KHZ);
-AF_DCMotor motor3(3, MOTOR34_1KHZ);
-AF_DCMotor motor4(4, MOTOR34_1KHZ);
-
-
-int distance = 0;
-int leftDistance;
-int rightDistance;
-boolean object;
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(irLeft, INPUT);
-  pinMode(irRight, INPUT);
-  servo.attach(10);
-  servo.write(90);
-
-  motor1.setSpeed(120);
-  motor2.setSpeed(120);
-  motor3.setSpeed(120);
-  motor4.setSpeed(120);
+int Set=15;
+int distance_L, distance_F, distance_R; 
+void setup(){ // put your setup code here, to run once
+Serial.begin(9600); // start serial communication at 9600bps
+pinMode(R_S, INPUT); // declare if sensor as input  
+pinMode(L_S, INPUT); // declare ir sensor as input
+pinMode(echo, INPUT );// declare ultrasonic sensor Echo pin as input
+pinMode(trigger, OUTPUT); // declare ultrasonic sensor Trigger pin as Output  
+pinMode(enA, OUTPUT); // declare as output for L298 Pin enA 
+pinMode(in1, OUTPUT); // declare as output for L298 Pin in1 
+pinMode(in2, OUTPUT); // declare as output for L298 Pin in2 
+pinMode(in3, OUTPUT); // declare as output for L298 Pin in3   
+pinMode(in4, OUTPUT); // declare as output for L298 Pin in4 
+pinMode(enB, OUTPUT); // declare as output for L298 Pin enB 
+analogWrite(enA, 200); // Write The Duty Cycle 0 to 255 Enable Pin A for Motor1 Speed 
+analogWrite(enB, 200); // Write The Duty Cycle 0 to 255 Enable Pin B for Motor2 Speed 
+pinMode(servo, OUTPUT);
+pinMode(rxPin, INPUT);
+pinMode(txPin, OUTPUT);
+ mySerial.begin(9600);
+ for (int angle = 70; angle <= 140; angle += 5)  {
+   servoPulse(servo, angle);  }
+ for (int angle = 140; angle >= 0; angle -= 5)  {
+   servoPulse(servo, angle);  }
+ for (int angle = 0; angle <= 70; angle += 5)  {
+   servoPulse(servo, angle);  }
+distance_F = Ultrasonic_read();
+delay(500);
 }
-
-void loop() {
-  if(plant_id=="one"){
-    driver(1);
-  }
-  if(plant_id=="two"){
-    driver(2);
-  }
-  if(plant_id=="three"){
-    driver(3);
-  }
+void loop(){  
+//==============================================
+//     Line Follower and Obstacle Avoiding
+//==============================================  
+distance_F = Ultrasonic_read();
+Serial.print("D F=");Serial.println(distance_F);
+//if Right Sensor and Left Sensor are at White color then it will call forword function
+ if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 0)){
+  if(distance_F > Set){forword();}
+                  else{Check_side();}  
+ }  
  
-}
-
-void driver(int count){
-   if (digitalRead(irLeft) == 0 && digitalRead(irRight) == 0 ) {
-    objectAvoid();
-    moveForward();
-    //forword
-  }
-  else if (digitalRead(irLeft) == 0 && digitalRead(irRight) == 1 ) {
-    objectAvoid();
-    Serial.println("TL");
-    //leftturn
-    moveLeft();
-  }
-  else if (digitalRead(irLeft) == 1 && digitalRead(irRight) == 0 ) {
-    objectAvoid();
-    Serial.println("TR");
-    //rightturn
-    moveRight();
-  }
-  else if (digitalRead(irLeft) == 1 && digitalRead(irRight) == 1 && count==1) {
-    count=0;
-    objectAvoid();
-    Serial.println("TL");
-    //leftturn
-    moveLeft();
+//if Right Sensor is Black and Left Sensor is White then it will call turn Right function
+else if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 0)){turnRight();}  
+//if Right Sensor is White and Left Sensor is Black then it will call turn Left function
+else if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 1)){turnLeft();} 
     
-  }
-  else if (digitalRead(irLeft) == 1 && digitalRead(irRight) == 1 && count==3) {
-    count=0;
-    objectAvoid();
-    Serial.println("TL");
-    //rightturn
-    moveRight();
-    
-  }
-  else if (digitalRead(irLeft) == 1 && digitalRead(irRight) == 1 && count==2) {
-    count=0;
-    objectAvoid();
-    //leftturn
-    moveForward();
-    
-  }
-  else if (digitalRead(irLeft) == 1 && digitalRead(irRight) == 1 && count==0) {
-
-    //Stop
-    Stop();
-  }
-
+delay(10);
 }
-
-void objectAvoid() {
-  distance = getDistance();
-  if (distance <= 15) {
-    //stop
-    Stop();
-    Serial.println("Stop");
-
-    lookLeft();
-    lookRight();
-    delay(100);
-    if (rightDistance <= leftDistance) {
-      //left
-      object = true;
-      turn();
-      Serial.println("moveLeft");
-    } else {
-      //right
-      object = false;
-      turn();
-      Serial.println("moveRight");
-    }
-    delay(100);
-  }
-  else {
-    //forword
-    Serial.println("moveforword");
-    moveForward();
-  }
+void servoPulse (int pin, int angle){
+int pwm = (angle*11) + 500;      // Convert angle to microseconds
+ digitalWrite(pin, HIGH);
+ delayMicroseconds(pwm);
+ digitalWrite(pin, LOW);
+ delay(50); // Refresh cycle of servo
 }
-
-int getDistance() {
-  delay(50);
-  int cm = sonar.ping_cm();
-  if (cm == 0) {
-    cm = 100;
-  }
-  return cm;
+//**********************Ultrasonic_read****************************
+long Ultrasonic_read(){
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  long time = pulseIn (echo, HIGH);
+  return time / 29 / 2;
 }
-
-int lookLeft () {
-  //lock left
-  servo.write(150);
+void compareDistance(){
+    if(distance_L > distance_R){
+  turnLeft();
   delay(500);
-  leftDistance = getDistance();
-  delay(100);
-  servo.write(90);
-  Serial.print("Left:");
-  Serial.print(leftDistance);
-  return leftDistance;
-  delay(100);
-}
-
-int lookRight() {
-  //lock right
-  servo.write(30);
+  forword();
+  delay(600);
+  turnRight();
   delay(500);
-  rightDistance = getDistance();
-  delay(100);
-  servo.write(90);
-  Serial.print("   ");
-  Serial.print("Right:");
-  Serial.println(rightDistance);
-  return rightDistance;
-  delay(100);
-}
-void Stop() {
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-  motor3.run(RELEASE);
-  motor4.run(RELEASE);
-}
-void moveForward() {
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
-}
-void moveBackward() {
-  motor1.run(BACKWARD);
-  motor2.run(BACKWARD);
-  motor3.run(BACKWARD);
-  motor4.run(BACKWARD);
-
-}
-void turn() {
-  if (object == false) {
-    Serial.println("turn Right");
-    moveLeft();
-    delay(700);
-    moveForward();
-    delay(800);
-    moveRight();
-    delay(900);
-    if (digitalRead(irRight) == 1) {
-      loop();
-    } else {
-      moveForward();
-    }
+  forword();
+  delay(600);
+  turnRight();
+  delay(400);
   }
-  else {
-    Serial.println("turn left");
-    moveRight();
-    delay(700);
-    moveForward();
-    delay(800);
-    moveLeft();
-    delay(900);
-    if (digitalRead(irLeft) == 1) {
-      loop();
-    } else {
-      moveForward();
-    }
+  else{
+  turnRight();
+  delay(500);
+  forword();
+  delay(600);
+  turnLeft();
+  delay(500);
+  forword();
+  delay(600);  
+  turnLeft();
+  delay(400);
   }
 }
-void moveRight() {
-  motor1.run(BACKWARD);
-  motor2.run(BACKWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
+void Check_side(){
+    Stop();
+    delay(100);
+ for (int angle = 70; angle <= 140; angle += 5)  {
+   servoPulse(servo, angle);  }
+    delay(300);
+    distance_R = Ultrasonic_read();
+    Serial.print("D R=");Serial.println(distance_R);
+    delay(100);
+  for (int angle = 140; angle >= 0; angle -= 5)  {
+   servoPulse(servo, angle);  }
+    delay(500);
+    distance_L = Ultrasonic_read();
+    Serial.print("D L=");Serial.println(distance_L);
+    delay(100);
+ for (int angle = 0; angle <= 70; angle += 5)  {
+   servoPulse(servo, angle);  }
+    delay(300);
+    compareDistance();
 }
-void moveLeft() {
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor3.run(BACKWARD);
-  motor4.run(BACKWARD);
+void forword(){  //forword
+digitalWrite(in1, LOW); //Left Motor backword Pin 
+digitalWrite(in2, HIGH); //Left Motor forword Pin 
+digitalWrite(in3, HIGH); //Right Motor forword Pin 
+digitalWrite(in4, LOW); //Right Motor backword Pin 
+}
+void backword(){ //backword
+digitalWrite(in1, HIGH); //Left Motor backword Pin 
+digitalWrite(in2, LOW); //Left Motor forword Pin 
+digitalWrite(in3, LOW); //Right Motor forword Pin 
+digitalWrite(in4, HIGH); //Right Motor backword Pin 
+}
+void turnRight(){ //turnRight
+digitalWrite(in1, LOW); //Left Motor backword Pin 
+digitalWrite(in2, HIGH); //Left Motor forword Pin 
+digitalWrite(in3, LOW); //Right Motor forword Pin 
+digitalWrite(in4, HIGH); //Right Motor backword Pin 
+}
+void turnLeft(){ //turnLeft
+digitalWrite(in1, HIGH); //Left Motor backword Pin 
+digitalWrite(in2, LOW); //Left Motor forword Pin 
+digitalWrite(in3, HIGH); //Right Motor forword Pin 
+digitalWrite(in4, LOW); //Right Motor backword Pin 
+}
+void Stop(){ //stop
+digitalWrite(in1, LOW); //Left Motor backword Pin 
+digitalWrite(in2, LOW); //Left Motor forword Pin 
+digitalWrite(in3, LOW); //Right Motor forword Pin 
+digitalWrite(in4, LOW); //Right Motor backword Pin 
 }
